b89dcc9ba7851f3dbdefb46a9230dfc30af8c3bf
Leonardo <lvneves2010@gmail.com>
2020-05-09 14:44:14 -0300

    Home + stats

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+//Temporario!!!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NCwibmFtZSI6Ikxlb25hcmRvIiwiZW1haWwiOiJsZW9uYXJkb0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTg4OTY5ODYwLCJleHAiOjE1ODkyMjkwNjB9.H34vr4RA9Z7Xt1mFuwy-_y7M6QM6jAPDggWSiG70iaw'
+
 new Vue({
   store,
   router,

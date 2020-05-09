80183aec45fba38e398ecfe551f60447c2a9be7f
Leonardo <lvneves2010@gmail.com>
2020-05-09 12:20:58 -0300

    icone de aba + header da aplicacao

---
+++ src/components/template/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside class="menu" v-show="isMenuVisible" >Menu</aside>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'Menu',
+    computed: mapState([ 'isMenuVisible' ])
+}
+</script>
+
+<style>
+    .menu {
+        grid-area: menu;
+        background: linear-gradient(to right, #011a26, #011f3e);
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
+</style>
